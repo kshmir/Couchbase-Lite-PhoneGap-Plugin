4bbde11e16154d0d50b2e6c01523b1e8caaac148
Cristian Pereyra <cristian@redmintlabs.com>
2015-06-08 14:05:22 -0300

    fixup ios and android code

--- src/android/CBLite.java
+++ src/android/CBLite.java
@@ -9,6 +9,7 @@ import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 
 import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.auth.BasicAuthenticator;
 import com.couchbase.lite.replicator.Replication;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.Database;
@@ -19,6 +20,7 @@ import com.couchbase.lite.router.URLStreamHandlerFactory;
 import com.couchbase.lite.View;
 import com.couchbase.lite.javascript.JavaScriptViewCompiler;
 import com.couchbase.lite.util.Log;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -30,153 +32,153 @@ import java.io.File;
 
 public class CBLite extends CordovaPlugin implements Replication.ChangeListener {
 
-	public static String TAG = "SyncGateway";
+    public static String TAG = "SyncGateway";
 
-	private static final int DEFAULT_LISTEN_PORT = 5984;
-	private static final String DATABASE_NAME = "contacts";
-	public static final String SYNC_URL = "http://192.168.1.104:4984/contacts";
-	private boolean initFailed = false;
-	private int listenPort;
-  private Credentials allowedCredentials;
-  private boolean inicializado = false;
-  private Manager manager;
-  private Database database;
+    private static final int DEFAULT_LISTEN_PORT = 5984;
+    private static final String DATABASE_NAME = "contacts";
+    public static final String SYNC_URL = "http://45.79.199.113:4988/contacts";
+    private boolean initFailed = false;
+    private int listenPort;
+    private Credentials allowedCredentials;
+    private boolean inicializado = false;
+    private Manager manager = null;
+    private Database database;
 
-	/**
-	 * Constructor.
-	 */
-	public CBLite() {
-		super();
-		System.out.println("CBLite() constructor called");
-	}
-
-	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-		System.out.println("initialize() called");
-
-		super.initialize(cordova, webView);
-		initCBLite();
-
-	}
-
-	private void initCBLite() {
-		try {
-
-		   allowedCredentials = new Credentials("admin", "admin");
-
-			URLStreamHandlerFactory.registerSelfIgnoreError();
-
-			View.setCompiler(new JavaScriptViewCompiler());
-
-			this.manager = startCBLite(this.cordova.getActivity());
-
-			this.database = this.manager.getDatabase(DATABASE_NAME);
-
-			startSync();
-
-			listenPort = startCBLListener(DEFAULT_LISTEN_PORT, this.manager, allowedCredentials);
-
-			inicializado = true;
-
-			System.out.println("initCBLite() completed successfully");
-
-
-		} catch (final Exception e) {
-			e.printStackTrace();
-			initFailed = true;
-		}
-
-	}
-
-	@Override
-	public boolean execute(String action, JSONArray args,
-			CallbackContext callback) {
-		if (action.equals("getURL")) {
-			try {
-
-				if (initFailed == true) {
-					callback.error("Failed to initialize couchbase lite.  See console logs");
-					return false;
-				} else {
-					String callbackRespone = String.format(
-							"http://%s:%s@localhost:%d/",
-                            allowedCredentials.getLogin(),
-                            allowedCredentials.getPassword(),
-                            listenPort
-                    );
-
-					callback.success(callbackRespone);
-
-					return true;
-				}
-
-			} catch (final Exception e) {
-				e.printStackTrace();
-				callback.error(e.getMessage());
-			}
-		}
-		return false;
-	}
-
-	protected Manager startCBLite(Context context) {
-		Manager manager;
-		try {
-		  Manager.enableLogging(Log.TAG, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
-			Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
-			manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
-			
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return manager;
-	}
-
-	private void startSync() {
-
-    URL syncUrl;
-    try {
-        syncUrl = new URL(SYNC_URL);
-    } catch (MalformedURLException e) {
-        throw new RuntimeException(e);
+    /**
+     * Constructor.
+     */
+    public CBLite() {
+        super();
+        System.out.println("CBLite() constructor called");
     }
 
-    Replication pullReplication = this.database.createPullReplication(syncUrl);
-    pullReplication.setContinuous(true);
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        System.out.println("initialize() called");
 
-    Replication pushReplication = this.database.createPushReplication(syncUrl);
-    pushReplication.setContinuous(true);
+        super.initialize(cordova, webView);
+        initCBLite();
 
-    pullReplication.start();
-    pushReplication.start();
+    }
 
-    pullReplication.addChangeListener(this);
-    pushReplication.addChangeListener(this);
+    private void initCBLite() {
+        try {
+            URLStreamHandlerFactory.registerSelfIgnoreError();
 
-  }
+            View.setCompiler(new JavaScriptViewCompiler());
 
-  @Override
+            this.manager = startCBLite(this.cordova.getActivity());
+
+            this.database = this.manager.getDatabase(DATABASE_NAME);
+
+            listenPort = startCBLListener(DEFAULT_LISTEN_PORT, this.manager, null);
+
+            inicializado = true;
+
+            System.out.println("initCBLite() completed successfully");
+
+
+        } catch (final Exception e) {
+            e.printStackTrace();
+            initFailed = true;
+        }
+
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args,
+                           CallbackContext callback) {
+        if (action.equals("startSync")) {
+            try {
+
+                String user = args.getString(0);
+                String password = args.getString(1);
+                String url = null;
+                if (args.length() > 2) {
+                    url = args.getString(2);
+                } else {
+                    url = null;
+                }
+
+                if (manager != null) {
+                    startSync(user, password, url);
+                }
+
+            } catch (final Exception e) {
+                e.printStackTrace();
+                callback.error(e.getMessage());
+            }
+        }
+        return false;
+    }
+
+    protected Manager startCBLite(Context context) {
+        Manager manager;
+        try {
+            Manager.enableLogging(Log.TAG, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_CHANGE_TRACKER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_BLOB_STORE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_LISTENER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_MULTI_STREAM_WRITER, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_REMOTE_REQUEST, Log.VERBOSE);
+            Manager.enableLogging(Log.TAG_ROUTER, Log.VERBOSE);
+            manager = new Manager(new AndroidContext(context), Manager.DEFAULT_OPTIONS);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return manager;
+    }
+
+    private void startSync(String user, String password, String optionalUrl) {
+
+        URL syncUrl;
+        try {
+            if (optionalUrl != null) {
+                syncUrl = new URL(optionalUrl);
+            } else {
+
+                syncUrl = new URL(SYNC_URL);
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        Replication pullReplication = this.database.createPullReplication(syncUrl);
+        pullReplication.setContinuous(true);
+
+        Replication pushReplication = this.database.createPushReplication(syncUrl);
+        pushReplication.setContinuous(true);
+
+        BasicAuthenticator authenticator = new BasicAuthenticator(user, password);
+        pullReplication.setAuthenticator(authenticator);
+        pushReplication.setAuthenticator(authenticator);
+
+        pullReplication.addChangeListener(this);
+        pushReplication.addChangeListener(this);
+
+        pullReplication.start();
+        pushReplication.start();
+
+    }
+
+    @Override
     public void changed(Replication.ChangeEvent event) {
 
-      Replication replication = event.getSource();
-      Log.d(TAG, "Replication : " + replication + " changed.");
-      if (!replication.isRunning()) {
-        String msg = String.format("Replicator %s not running", replication);
-        Log.d(TAG, msg);
-      }
-      else {
-        int processed = replication.getCompletedChangesCount();
-        int total = replication.getChangesCount();
-        String msg = String.format("Replicator processed %d / %d", processed, total);
-        Log.d(TAG, msg);
-      }
+        Replication replication = event.getSource();
+        Log.d(TAG, "Replication : " + replication + " changed.");
+        if (!replication.isRunning()) {
+            String msg = String.format("Replicator %s not running", replication);
+            Log.d(TAG, msg);
+        } else {
+            int processed = replication.getCompletedChangesCount();
+            int total = replication.getChangesCount();
+            String msg = String.format("Replicator processed %d / %d", processed, total);
+            Log.d(TAG, msg);
+        }
 
       /*if (event.getError() != null) {
         showError("Sync error", event.getError());
@@ -184,21 +186,21 @@ public class CBLite extends CordovaPlugin implements Replication.ChangeListener 
 
     }
 
-	private int startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) {
-		LiteListener listener = new LiteListener(manager, listenPort, allowedCredentials);
-		int boundPort = listener.getListenPort();
-		Thread thread = new Thread(listener);
-		thread.start();
-		return boundPort;
+    private int startCBLListener(int listenPort, Manager manager, Credentials allowedCredentials) {
+        LiteListener listener = new LiteListener(manager, listenPort, allowedCredentials);
+        int boundPort = listener.getListenPort();
+        Thread thread = new Thread(listener);
+        thread.start();
+        return boundPort;
 
-	}
+    }
 
-	public void onResume(boolean multitasking) {
-		System.out.println("CBLite.onResume() called");
-	}
+    public void onResume(boolean multitasking) {
+        System.out.println("CBLite.onResume() called");
+    }
 
-	public void onPause(boolean multitasking) {
-		System.out.println("CBLite.onPause() called");
-	}
+    public void onPause(boolean multitasking) {
+        System.out.println("CBLite.onPause() called");
+    }
 
 }
--- src/ios/CBLite.m
+++ src/ios/CBLite.m
@@ -1,12 +1,32 @@
 #import "CBLite.h"
 
 #import "CouchbaseLite.h"
+#import "CBLAuthenticator.h"
 #import "CBLListener.h"
 #import "CBLRegisterJSViewCompiler.h"
 
 #import <Cordova/CDV.h>
 
-@implementation CBLite
+// The name of the local database the app will create. This name is mostly arbitrary, but must not
+// be changed after deploying your app, or users will lose their data!
+// (Note that database names cannot contain uppercase letters.)
+#define kDatabaseName @"contacts"
+
+// The remote database URL to sync with. This is preconfigured with a sample database we maintain.
+// In your own apps you will of course want to set this to a database you run, on your own Sync
+// Gateway instance.
+#define kServerDbURL @"http://45.79.199.113:4988/contacts"
+
+
+@implementation CBLite {
+  __strong NSString * username;
+  __strong NSString * password;
+  NSURL* remoteSyncURL;
+  CBLReplication* _pull;
+  CBLReplication* _push;
+  NSError* _syncError;
+  CBLDatabase * database;
+}
 
 @synthesize liteURL;
 
@@ -26,11 +46,36 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:urlCommand.callbackId];
 }
 
+- (void)startSync:(CDVInvokedUrlCommand*)urlCommand
+{
+    username = [urlCommand argumentAtIndex:0];
+    password = [urlCommand argumentAtIndex:1];
+    NSString * syncUrl = kServerDbURL;
+    if ([[urlCommand arguments] count] > 2) {
+      syncUrl = [urlCommand argumentAtIndex:2];
+    }
+
+    NSURL* serverDbURL = [NSURL URLWithString: syncUrl];
+    _pull = [database createPullReplication: serverDbURL];
+    _push = [database createPushReplication: serverDbURL];
+    id<CBLAuthenticator> auth = (id<CBLAuthenticator>) [CBLAuthenticator basicAuthenticatorWithName:username password:password];
+    _pull.authenticator = auth;
+    _push.authenticator = auth;
+    _pull.continuous = _push.continuous = YES;
+  
+    [_push start];
+    [_pull start];
+  
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:urlCommand.callbackId];
+}
 
 - (void)launchCouchbaseLite
 {
     NSLog(@"Launching Couchbase Lite...");
-    CBLManager* dbmgr = [CBLManager sharedInstance];
+    NSError* error;
+    database = [[CBLManager sharedInstance] databaseNamed: kDatabaseName error: &error];
+  
     CBLRegisterJSViewCompiler();
 #if 1
     // Couchbase Lite 1.0's CBLRegisterJSViewCompiler function doesn't register the filter compiler
@@ -39,8 +84,11 @@
         [CBLDatabase setFilterCompiler: [[cblJSFilterCompiler alloc] init]];
     }
 #endif
-    self.liteURL = dbmgr.internalURL;
+    CBLListener * listener =[[CBLListener alloc] initWithManager:[CBLManager sharedInstance] port:5984];
+    self.liteURL = listener.URL;
+    [listener start:&error];
     NSLog(@"Couchbase Lite url = %@", self.liteURL);
 }
 
 @end
+
